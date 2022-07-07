d2f65d0c378cba741e9a4f26777f5e844f7c2dff
Solaire-Kun <vinciguerraluca03@hotmail.com>
2022-07-07 23:19:03 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,13 +1,25 @@
 const fieldSearch = document.querySelector('[data-search-field]');
 const buttonSearch = document.querySelector('[data-search-button]');
 const searching = document.querySelector('[data-searching]');
+const noResults = document.querySelector('[data-no-results]');
+const ERROR = document.querySelector('[data-error]');
 const bookContainer = document.querySelector('[data-book-container]');
 const bookTemplate = document.querySelector('[data-book-template]');
 
 function searchBook(query) {
   const url = `https://openlibrary.org/search.json?q=${query}`;
   fetch(url)
-    .then(resolve => resolve.json())
+    .then(resolve => {
+      if(resolve.ok) {
+        return resolve.json();
+      }else {
+        throw new Error();
+      };
+    })
+    .catch(err => {
+      searching.textContent = '';
+      ERROR.textContent = 'Something went wrong: ' + err;
+    })
     .then((jsonData) => {
       for(let i = 0; i <= 10; i++) {
         const item = jsonData.docs[i];
@@ -17,12 +29,17 @@ function searchBook(query) {
           const bookAuthor = newBook.querySelector('[data-author]');
           const bookKey = newBook.querySelector('[data-key]');
 
+          bookContainer.classList.replace('d-none', 'd-block');
           bookTitle.textContent = item.title;
           bookAuthor.textContent = 'Author(s): ' + item.author_name;
-          bookKey.innerHTML = 'View Description';
+          bookKey.textContent = 'View Description';
           bookKey.setAttribute('href', `https://openlibrary.org${item.key}`);
           bookContainer.append(newBook);
           searching.textContent = '';
+        }else {
+          searching.textContent = '';
+          noResults.textContent = 'No results found.';
+          break;
         };
       };
     });
@@ -36,11 +53,14 @@ fieldSearch.onkeypress = (event) => {
 
 buttonSearch.onclick = () => {
     if(fieldSearch.value != '') {
+        searching.textContent = 'Searching...';
+        ERROR.textContent = '';
+        noResults.textContent = '';
+        bookContainer.classList.replace('d-block', 'd-none');
         searchBook(fieldSearch.value);
         while(bookContainer.lastChild) {
               bookContainer.removeChild(bookContainer.lastChild);
         };
-        searching.textContent = 'Searching...';
     }else {
         alert("You can't perform a search with an empty search field");
     };
